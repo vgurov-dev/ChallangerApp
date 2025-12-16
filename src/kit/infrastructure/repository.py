919091db548266356a842b50b7de0bb3df23028d
Vladislav Gurov <vgurov.dev@gmail.com>
2025-12-16 15:08:10 +0300

    Added kit for common usage and repository pattern

---
+++ src/kit/infrastructure/repository.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+
+class Repository[T](ABC):
+    """
+    Abstract base class for repositories.
+    """
+    @abstractmethod
+    def get_bi_id(
+            self,
+            id: str | int
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all(self, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_one(self,
+                row: T):
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_many(
+            self,
+            rows: Iterable[T]
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_one(self, id):
+        raise NotImplementedError
+
+
+
